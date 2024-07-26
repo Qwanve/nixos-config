315ca8a8268de83ec04e291b5dabb9a674fc6a37
Daniel Stroh <strohdaniel624@gmail.com>
2024-07-25 20:33:06 -0500

    Update catppuccin foot theme

--- home/gui.nix
+++ home/gui.nix
@@ -16,7 +16,7 @@
     # server.enable = true;
     settings = {
       main = {
-        include = "${inputs.catppuccin-foot-theme}/catppuccin-${catppuccin.lower.theme}.ini";
+        include = "${inputs.catppuccin-foot-theme}/themes/catppuccin-${catppuccin.lower.theme}.ini";
         shell = "${pkgs.fish}/bin/fish";
         dpi-aware = true;
         
