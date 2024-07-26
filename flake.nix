878f0668d6f3648c8a2c4a92966eb542a4eb55fc
Daniel Stroh <strohdaniel624@gmail.com>
2024-07-25 20:38:15 -0500

    Update flake.lock, move to lix, add plasma-manager

--- flake.nix
+++ flake.nix
@@ -5,8 +5,16 @@
     # NixOS official package source, using the nixos-24.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    plasma-manager.url = "github:nix-community/plasma-manager";
+    plasma-manager.inputs.nixpkgs.follows = "nixpkgs";
+    plasma-manager.inputs.home-manager.follows = "home-manager";
 
     catppuccin-foot-theme.url = "github:catppuccin/foot";
     catppuccin-foot-theme.flake = false;
@@ -18,7 +26,7 @@
     chromebook-ucm-conf.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... } @ inputs: {
+  outputs = { self, nixpkgs, home-manager, plasma-manager, ... } @ inputs: {
     nixosConfigurations.nyx = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs.inputs = inputs;
@@ -28,9 +36,12 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
           home-manager.users.chrx = import ./nyx/home.nix;
           home-manager.extraSpecialArgs.inputs = inputs;
         }
+
+        inputs.lix-module.nixosModules.default
 
         {
           nix.channel.enable = false;
