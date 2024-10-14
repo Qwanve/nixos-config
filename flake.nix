8bf258046a9205b9ac037079a7b993a3a7fd394b
Daniel Stroh <strohdaniel624@gmail.com>
2024-10-14 14:09:42 -0500

    Switch back to nix

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
     # NixOS official package source, using the nixos-24.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # lix-module = {
+    #   url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -48,7 +48,7 @@
           home-manager.extraSpecialArgs.inputs = inputs;
         }
 
-        inputs.lix-module.nixosModules.default
+        # inputs.lix-module.nixosModules.default
 
         {
           nix.channel.enable = false;
