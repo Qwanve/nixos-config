85ceed94fd3d9bbc35f91fc2772ef434fd8fa739
Qwanve <qwanve@proton.me>
2025-09-07 11:29:04 -0500

    Update kernel

--- nyx/system.nix
+++ nyx/system.nix
@@ -36,7 +36,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_6_1;
+  # boot.kernelPackages = pkgs.linuxPackages_6_1;
   boot.kernelParams = [
     "mem_sleep_default=deep"
   ];
