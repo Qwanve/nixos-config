58c782cc8f3c66799c0ad151ef9ce5f8e4dc9efa
Qwanve <qwanve@proton.me>
2026-01-18 10:35:30 -0600

    Delete eww-config?

--- home/eww-config/scripts/watchmicmute.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-pactl get-source-mute @DEFAULT_SOURCE@ | rg -o "yes|no"
-pactl subscribe | rg "source" --line-buffered | xargs -L1 pactl get-source-mute @DEFAULT_SOURCE@ | rg -o "yes|no" --line-buffered
